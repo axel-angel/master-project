9e8bb49a2b4dad29a0327cd9430b00cd83520d81
Axel Angel <axel+git@vneko.ch>
2015-02-24 20:56:31 +0100

    Fix convnet_test.py for MNIST

--- src/convnet_test.py
+++ src/convnet_test.py
@@ -12,46 +12,51 @@ np.set_printoptions(suppress=True) # no sci notation
 parser = argparse.ArgumentParser()
 parser.add_argument('--proto', type=str, help='deploy .prototxt', required=True)
 parser.add_argument('--model', type=str, help='.caffemodel', required=True)
-parser.add_argument('--mean', type=str, help='mean .npy', required=True)
-parser.add_argument('--path', type=str, help='directory', required=True)
-parser.add_argument('--threshold', type=float, help='maxarg', default=0.5)
-parser.add_argument('--filelist', type=str,
-        help='file list with labels', required=True)
-parser.add_argument('--dimensions', type=int, nargs='+',
-        help='image dimension', required=True)
+#parser.add_argument('--mean', type=str, help='mean .npy', default=None)
+#parser.add_argument('--threshold', type=float, help='maxarg', default=0.5)
+parser.add_argument('--npz', type=str,
+        help='numpy arrays data + labels', required=True)
+#parser.add_argument('--dimensions', type=int, nargs='+',
+#        help='image dimension', required=True)
 
 args = parser.parse_args()
 
 caffe.set_mode_cpu()
-caffe.set_phase_test()
+#caffe.set_phase_test()
 
-dims = tuple(args.dimensions) + (3,)
-m = caffe.Classifier(args.proto, args.model,
-        image_dims=dims, channel_swap=(2,1,0), raw_scale=255,
-        mean=np.load(args.mean))
+X = np.load(args.npz)
+data = X['arr_0']
+labels = X['arr_1']
+
+labels_set = set()
+
+assert data.shape[0] == labels.shape[0]
+count = data.shape[0]
+
+dims = (data.shape[1], data.shape[2])
+m = caffe.Net(args.proto, args.model)
 
 matrix = defaultdict(int) # (real,pred) -> int
-labels = set()
 
-for line in open(args.filelist, 'r'):
-    fpath, label = map(lambda (f,x): f(x), zip([str, int], line.split(' ', 1)))
-    img = caffe.io.load_image(args.path + fpath)
-    pred = m.predict([img])
+for i in range(count):
+    label = labels[i]
+    out = m.forward_all(data=np.asarray([ np.array([ data[i] ]) ]))
 
     # our pred is the one above threshold else 0 (bias default)
-    maxpred = np.max(pred)
-    plabel = np.argmax(pred) if maxpred > args.threshold else 0
+    #maxpred = np.max(pred)
+    #plabel = np.argmax(pred)
+    plabel = int(np.argmax(out['prob'][0], axis=0))
 
-    iscorrect = plabel == label
+    iscorrect = plabel == labels[i]
     matrix[(label, plabel)] += 1
-    labels.update([label, plabel])
+    labels_set.update([label, plabel])
 
     if not iscorrect:
-        print "\rError: expected %i, got %i, for %s (pred: %s)" \
-                % (label, plabel, fpath, pred)
+        print "\rError: expected %i, got %i, for %i (pred: %s)" \
+                % (label, plabel, i, out['prob'][0].tolist())
 
     def cc_gen():
-        for l in labels:
+        for l in labels_set:
             crt = matrix[(l,l)]
             cnt = sum(x for (l2,pl), x in matrix.iteritems() if l2 == l)
             pre = sum(x for (l2,pl), x in matrix.iteritems() if pl == l)
@@ -73,6 +78,6 @@ for line in open(args.filelist, 'r'):
 print ""
 print "Confusion matrix:"
 print "(r , p) | count"
-for l in labels:
-    for pl in labels:
+for l in labels_set:
+    for pl in labels_set:
         print "(%i , %i) | %i" % (l, pl, matrix[(l,pl)])
