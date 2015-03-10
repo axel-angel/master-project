a1c053a9bc72453fc71c3ab6d59605ee018c50e1
Axel Angel <axel+git@vneko.ch>
2015-03-10 17:30:37 +0100

    utils: Move distortions in utils, simplify namespace/piecewise_affine

--- src/piecewise_affine.py
+++ src/piecewise_affine.py
@@ -4,46 +4,29 @@ from skimage.transform import PiecewiseAffineTransform, warp
 from skimage.io import imread
 from sys import argv
 from scipy.ndimage.interpolation import rotate
-
-axis = 1
+from utils import img_sindisp_x, img_sindisp_y
 
 def transfo(i, v, axis=0):
     if axis == 0:
-        i = rotate(i, 90)
-
-    rows, cols = i.shape[0], i.shape[1]
-
-    src_cols = np.linspace(0, cols, 28)
-    src_rows = np.linspace(0, rows, 28)
-    src_rows, src_cols = np.meshgrid(src_rows, src_cols)
-    src = np.dstack([src_cols.flat, src_rows.flat])[0]
-
-    # add sinusoidal oscillation to row coordinates
-    dst_rows = src[:, 1] - np.sin(np.linspace(0, 3 * np.pi, src.shape[0])) * v
-    dst_cols = src[:, 0]
-    dst_rows -= v / 2
-    dst = np.vstack([dst_cols, dst_rows]).T
-
-
-    tform = PiecewiseAffineTransform()
-    tform.estimate(src, dst)
-
-    out = warp(i, tform, output_shape=(rows, cols))
-    if axis == 0:
-        out = rotate(out, -90)
-    return out
+        return img_sindisp_x(i, v)
+    else:
+        return img_sindisp_y(i, v)
 
 if __name__ == '__main__':
     imgs = map(imread, argv[1:])
 
+    print "Compute images"
     X = []
-    for i in imgs:
+    for j, i in enumerate(imgs):
+        print "image: %i/%i" % (j, len(imgs))
         for axis in [0,1]:
             x = []
             for v in xrange(-6, 7, 2):
+                print "\ttransfo: axis=%i v=%i" % (axis, v)
                 x.append(transfo(i, v, axis=axis))
             X.append(x)
 
+    print "Prepare plot"
     for i, x in enumerate(X):
         X[i] = np.concatenate(x, axis=1)
     Y = np.concatenate(X, axis=0)
--- src/utils.py
+++ src/utils.py
@@ -2,21 +2,42 @@
 # -*- coding: utf-8 -*-
 
 import numpy as np
-import scipy.ndimage
+from scipy.ndimage.interpolation import rotate, shift
+from scipy.ndimage.filters import gaussian_filter
 import scipy.misc
+from skimage.transform import PiecewiseAffineTransform, warp
 
 def img_shift_x(i, v):
-    return scipy.ndimage.interpolation.shift(i, (v,0))
+    return shift(i, (v,0))
 def img_shift_y(i, v):
-    return scipy.ndimage.interpolation.shift(i, (0,v))
+    return shift(i, (0,v))
 def img_blur(i, v):
-    return scipy.ndimage.filters.gaussian_filter(i, v)
+    return gaussian_filter(i, v)
 def img_rotate(i, v):
-    return scipy.ndimage.interpolation.rotate(i, v, reshape=False)
+    return rotate(i, v, reshape=False)
 def img_scale(i, v):
     return np.clip(i * v, 0, 255)
 
+def img_sindisp_x(i, v):
+    "sinusoidal displacement along x-axis"
+    return rotate(img_sindisp_y(rotate(i, 90), v), -90)
 
+def img_sindisp_y(i, v):
+    "sinusoidal displacement along y-axis"
+    rows, cols = i.shape[0], i.shape[1]
 
+    src_cols = np.linspace(0, cols, 10)
+    src_rows = np.linspace(0, rows, 10)
+    src_rows, src_cols = np.meshgrid(src_rows, src_cols)
+    src = np.dstack([src_cols.flat, src_rows.flat])[0]
 
+    # add sinusoidal oscillation to row coordinates
+    dst_rows = src[:, 1] - np.sin(np.linspace(0, 3 * np.pi, src.shape[0])) * v
+    dst_cols = src[:, 0]
+    dst_rows -= v / 2
+    dst = np.vstack([dst_cols, dst_rows]).T
 
+    tform = PiecewiseAffineTransform()
+    tform.estimate(src, dst)
+
+    return warp(i, tform, output_shape=(rows, cols))
