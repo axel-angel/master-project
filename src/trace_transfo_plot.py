954834184bec7835d1811e0f54b99fd20b2b59e0
Axel Angel <axel+git@vneko.ch>
2015-03-16 18:11:57 +0100

    trace_transfo_plot: Adapt to v2 (WIP)

--- src/trace_transfo_plot.py
+++ src/trace_transfo_plot.py
@@ -10,7 +10,7 @@ if __name__ == "__main__":
     import argparse
     parser = argparse.ArgumentParser()
     parser.add_argument('--in-npz', type=str, required=True)
-    parser.add_argument('--transfo', type=str, default=None)
+    parser.add_argument('--filter', type=str, required=True)
     parser.add_argument('--out', type=str, required=True)
     args = parser.parse_args()
 
@@ -20,34 +20,43 @@ if __name__ == "__main__":
     imgs_tr_np = npz['imgs_tr_np']
     tsne = npz['tsne']
     pts = npz['pts']
-    labels = npz['labels']
     infos = npz['infos']
-    tr_map = npz['tr_map'].flat.next()
+
+    label_set = set(i['l'] for i in infos if i['src'] == "dataset")
+    trans_set = set(i['tr'] for i in infos if i['src'] != "dataset")
+    label_max = max(label_set)
+    tr_map = { k:label_max+1+i for i, k in enumerate(trans_set) }
 
     print "Filtering data"
-    Y = np.array([pts, labels, infos]).T
-    if args.transfo:
-        Y2 = filter(lambda x: x[1] == tr_map[args.transfo], Y)
-    else:
-        Y2 = filter(lambda x: x[2] != "dataset", Y)
+    Y = np.array([pts, infos]).T
+    Y2 = filter(lambda y: args.filter in y[1]['src'], Y)
 
     ds_annotates = set()
-    for [pt, label, info] in Y:
-        if info == "dataset" and label not in ds_annotates:
-            Y2.append([pt, label, "dataset %i" % (label)])
+    for [pt, info] in Y:
+        label = info['l']
+        if info['src'] == "dataset" and label not in ds_annotates:
+            Y2.append([ pt, info ])
             ds_annotates.add(label)
 
     print "Make plot"
+    colors = [ i['l'] if i['l'] >= 0 else tr_map[i['tr']] for y in Y2 ]
     plt.figure(figsize=(30, 30))
-    plt.scatter(pts[:,0], pts[:,1], s=5, cmap='bwr', c=labels, edgecolors='none')
+    plt.scatter(pts[:,0], pts[:,1], s=5, cmap='bwr', c=colors, edgecolors='none')
     bbox = dict(boxstyle = 'round,pad=0.5', fc = 'yellow', alpha = 0.5)
     arrowprops = dict(arrowstyle = '-', connectionstyle = 'arc3,rad=0', linewidth=0.2)
-    for i, [pt, label, info] in enumerate(Y2):
+    for i, [pt, info] in enumerate(Y2):
+        label = info['l']
         tsize = 40 + random() * 5
         tx = tsize * cos(i / 4.)
         ty = tsize * sin(i / 4.)
+
+        if info['v'] == 0:
+            txt = "D%s" % (label)
+        else:
+            txt = "I%s_%s(%i)" % (info['src'], info['tr'], info['v'])
+
         plt.annotate(
-            info,
+            txt,
             xy=pt, xytext=(tx, ty),
             textcoords = 'offset points', ha = 'right', va = 'bottom',
             bbox = None, arrowprops = arrowprops,
