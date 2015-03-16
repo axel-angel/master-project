fa27abc02d2d60da6f144bccd9e08cdbb2e67ac7
Axel Angel <axel+git@vneko.ch>
2015-03-16 18:12:23 +0100

    gen_lmdb_transfo: Create LMDB training set with distortions

---
+++ src/gen_lmdb_transfo.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python
+
+import sys
+import caffe
+import numpy as np
+import lmdb
+import argparse
+import utils
+
+def parse_transfo(s):
+    try:
+        xs = s.split(':', 3)
+        fmt = [str, int, int, int]
+        return map(lambda (f,x): f(x), zip(fmt, xs))
+    except:
+        raise argparse.ArgumentTypeError("Invalid")
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--in-npz', type=str, required=True)
+    parser.add_argument('--out-lmdb', type=str, required=True)
+    parser.add_argument('--transfo', type=parse_transfo, action='append',
+            default=[])
+    args = parser.parse_args()
+
+    print "Load dataset"
+    X = np.load(args.in_npz)
+    xs = X['arr_0']
+    ls = X['arr_1']
+    count = xs.shape[0]
+
+    trs = [ { 'f': getattr(utils, 'img_%s' % (tr)), 'steps': range(x,y,z) }
+            for (tr,x,y,z) in args.transfo ]
+    trs.append({ 'f': lambda i,v: i, 'steps': [0] })
+    trs_len = len(trs)
+    print "Transformations: %s" % ("\n\t".join(map(repr, trs)))
+
+    print "Write LMDB"
+    lmdb_env = lmdb.open(args.out_lmdb, map_size=1e12)
+    with lmdb_env.begin(write=True) as lmdb_txn:
+        for i, (x, l) in enumerate(np.array([ xs, ls ]).T):
+            trf = [ (t['f'], v) for t in trs for v in t['steps'] ]
+            for j, (f, v) in enumerate(trf):
+                i2 = i * trs_len + j
+                x2 = f(x, v).reshape((1,) + x.shape)
+                datum = caffe.io.array_to_datum(x2, label=int(l))
+                lmdb_txn.put("%010d" % (i2), datum.SerializeToString())
+
+            if i%100 == 0:
+                sys.stdout.write("Progress %4.1f%% (%i/%i)\r" \
+                        % (100.*i/count, i, count))
+                sys.stdout.flush()
+
+    print "\nClose database"
+    lmdb_env.close()
