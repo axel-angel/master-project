3914c1641838d001ccc63acadb93cc81772c7803
Axel Angel <axel+git@vneko.ch>
2015-03-17 10:53:45 +0100

    convnet_test_lmdb: Improve, Add confmatrix, Cleaner output

--- src/convnet_test_lmdb.py
+++ src/convnet_test_lmdb.py
@@ -1,9 +1,9 @@
 import sys
 import caffe
-import matplotlib
 import numpy as np
 import lmdb
 import argparse
+from collections import defaultdict
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -12,29 +12,45 @@ if __name__ == "__main__":
     parser.add_argument('--lmdb', type=str, required=True)
     args = parser.parse_args()
 
+    count = 0
+    correct = 0
+    matrix = defaultdict(int) # (real,pred) -> int
+    labels_set = set()
+
     net = caffe.Net(args.proto, args.model, caffe.TEST)
     caffe.set_mode_cpu()
     lmdb_env = lmdb.open(args.lmdb)
     lmdb_txn = lmdb_env.begin()
     lmdb_cursor = lmdb_txn.cursor()
-    count = 0
-    correct = 0
+
     for key, value in lmdb_cursor:
-        count = count + 1
         datum = caffe.proto.caffe_pb2.Datum()
         datum.ParseFromString(value)
         label = int(datum.label)
         image = caffe.io.datum_to_array(datum)
         image = image.astype(np.uint8)
+
         out = net.forward_all(data=np.asarray([image]))
-        predicted_label = out['prob'][0].argmax(axis=0)
-        if label == predicted_label[0][0]:
-            correct = correct + 1
-        else:
-            print("\rError: expected %i but predicted %i" \
-                    % (label, predicted_label[0][0]))
+        plabel = int(out['prob'][0].argmax(axis=0))
+
+        count += 1
+        iscorrect = label == plabel
+        correct += (1 if iscorrect else 0)
+        matrix[(label, plabel)] += 1
+        labels_set.update([label, plabel])
+
+        if not iscorrect:
+            print("\rError: key=%s, expected %i but predicted %i" \
+                    % (key, label, plabel))
 
         sys.stdout.write("\rAccuracy: %.1f%%" % (100.*correct/count))
         sys.stdout.flush()
 
-    print(str(correct) + " out of " + str(count) + " were classified correctly")
+    print(", %i/%i corrects" % (correct, count))
+
+    print ""
+    print "Confusion matrix:"
+    print "(r , p) | count"
+    for l in labels_set:
+        for pl in labels_set:
+            print "(%i , %i) | %i" % (l, pl, matrix[(l,pl)])
