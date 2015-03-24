d840e78651e466441db95c5f2bf47b01df89a8f6
Axel Angel <axel+git@vneko.ch>
2015-03-24 14:25:09 +0100

    tsne_convert_js: Add base64 jpg image (visible in plot_interactive)

--- src/tsne_convert_js.py
+++ src/tsne_convert_js.py
@@ -7,21 +7,34 @@ from math import sin, cos
 from utils import partition
 import json
 from collections import defaultdict
+import lmdb
+import caffe
+from base64 import b64encode
+from scipy.misc import imsave
+from io import BytesIO
+from utils import *
 
 def conv(d): # legacy conversion of label to int
     d['l'] = int(d['l'])
     return d
+
+def img_encode(i):
+    bio = BytesIO()
+    imsave(bio, flat_shape(img.astype(np.uint8)), format='jpeg')
+    return b64encode(bio.getvalue())
 
 if __name__ == "__main__":
     import argparse
     parser = argparse.ArgumentParser()
     parser.add_argument('--in-npz', type=str, required=True)
     parser.add_argument('--out-js', type=str, required=True)
+    parser.add_argument('--lmdb', type=str, required=True)
     args = parser.parse_args()
 
     X = np.load(args.in_npz)
     Y = np.array([ X['pts'], X['infos'], range(X['pts'].shape[0]) ]).T
 
+    # stats per src, label, transformations
     src_set = defaultdict(int)
     label_set = defaultdict(int)
     tr_set = defaultdict(int)
@@ -30,14 +43,31 @@ if __name__ == "__main__":
         label_set[str(info['l'])] += 1
         tr_set[info['tr']] += 1
 
+    # load dataset images (encode in base64 for easy HTML inclusion)
+    lmdb_env = lmdb.open(args.lmdb)
+    lmdb_txn = lmdb_env.begin()
+    lmdb_cursor = lmdb_txn.cursor()
+    imgs = []
+    for key, value in lmdb_cursor:
+        datum = caffe.proto.caffe_pb2.Datum()
+        datum.ParseFromString(value)
+        img = caffe.io.datum_to_array(datum)
+        imgs.append(img_encode(img)) # include dataset set
+    for img in X['imgs_tr_np']:
+        imgs.append(img_encode(img)) # include distorted set too
+
+    # convert to json-serialisable
     ds = [ dict(x=pt[0], y=pt[1], i=i, **conv(info)) for pt, info, i in Y ]
     with open(args.out_js, 'wb') as fd:
         fd.write('var X = ')
         json.dump(ds, fd)
         fd.write(';')
 
+        fd.write('var imgs = ')
+        json.dump(imgs, fd)
+        fd.write(';')
+
         for v in ['src_set', 'label_set', 'tr_set']:
             fd.write('var %s = ' % (v))
-            print locals()[v]
             json.dump(locals()[v], fd)
             fd.write(';')
