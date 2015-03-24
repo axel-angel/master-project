6fe6704a471dcf8b3e0133d74febfbecd9915397
Axel Angel <axel+git@vneko.ch>
2015-03-24 11:13:24 +0100

    tsne_convert_js: Compute sets and index (adapt plot_interactive)

--- src/tsne_convert_js.py
+++ src/tsne_convert_js.py
@@ -6,6 +6,7 @@ from random import random
 from math import sin, cos
 from utils import partition
 import json
+from collections import defaultdict
 
 def conv(d): # legacy conversion of label to int
     d['l'] = int(d['l'])
@@ -19,10 +20,24 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     X = np.load(args.in_npz)
-    Y = np.array([ X['pts'], X['infos'] ]).T
+    Y = np.array([ X['pts'], X['infos'], range(X['pts'].shape[0]) ]).T
 
-    ds = [ dict(x=pt[0], y=pt[1], **conv(i)) for pt, i in Y ]
+    src_set = defaultdict(int)
+    label_set = defaultdict(int)
+    tr_set = defaultdict(int)
+    for _, info, _ in Y:
+        src_set[info['src']] += 1
+        label_set[str(info['l'])] += 1
+        tr_set[info['tr']] += 1
+
+    ds = [ dict(x=pt[0], y=pt[1], i=i, **conv(info)) for pt, info, i in Y ]
     with open(args.out_js, 'wb') as fd:
         fd.write('var X = ')
         json.dump(ds, fd)
         fd.write(';')
+
+        for v in ['src_set', 'label_set', 'tr_set']:
+            fd.write('var %s = ' % (v))
+            print locals()[v]
+            json.dump(locals()[v], fd)
+            fd.write(';')
