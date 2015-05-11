ad57a2c2bdf6c5e774bf498092b3b4f24ba5e30d
Axel Angel <axel+git@vneko.ch>
2015-05-11 21:17:38 +0200

    gen_transfo_siamese1: Implemented

--- src/gen_transfo_siamese1.py
+++ src/gen_transfo_siamese1.py
@@ -9,42 +9,66 @@ import utils
 from random import randint
 import multiprocessing
 from utils import *
+from collections import defaultdict
+from random import Random
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('--in-npz', type=str, required=True)
     parser.add_argument('--out-npz', type=str, required=True)
+    parser.add_argument('--num', type=int, required=True)
+    parser.add_argument('--label', type=int, nargs='+', required=True)
     args = parser.parse_args()
 
     print "Load dataset"
-    X = np.load(args.in_npz)
-    xs = X['arr_0']
-    ls = X['arr_1']
-    count = xs.shape[0]
+    npz = np.load(args.in_npz)
+    X = npz['arr_0']
+    ls = npz['arr_1']
+    count = X.shape[0]
 
     trs = [2, 4]
     print "Transformations:\n\t%s" % ("\n\t".join(map(repr, trs)))
 
     def process( (x, l) ):
-        xs = [ (l, x, 0) ]
+        xs = []
+        for v in reversed(trs):
+            xs.append( (l, img_shift_x(x, -v), -v) )
+        xs.append( (l, x, 0) )
         for v in trs:
             xs.append( (l, img_shift_x(x, +v), +v) )
-            xs.append( (l, img_shift_x(x, -v), -v) )
         return xs
 
     print "Generate images"
     num_cores = multiprocessing.cpu_count()
     pool = multiprocessing.Pool(num_cores)
     res = []
-    itr = pool.imap_unordered(process, np.array([ xs, ls ]).T)
-    for i, x in enumerate(itr):
-        res.append(x)
-        sys.stdout.write("\rRunning: %i" % (i))
+    Xls = filter(lambda (x,l): l in args.label, np.array([ X, ls ]).T)
+    itr = pool.imap_unordered(process, Xls)
+    for i, xs in enumerate(itr):
+        res.append(xs)
+        sys.stdout.write("\rTransforming: %.0f%%" % (i * 100. / len(Xls)))
         sys.stdout.flush()
     pool.terminate()
     print ""
 
+    rand = Random(1)
+    out_imgs = []
+    out_labels = []
+    for i in xrange(args.num):
+        xs, ys = rand.sample(res, k=2)
+        # pick a similar pair
+        idx = rand.randint(0, len(xs) - 2)
+        (l1, i1, v1), (l2, i2, v2) = xs[idx:idx+2]
+        out_imgs.append( np.array([ i1, i2 ]) )
+        out_labels.append( 1 )
+        # pick a disimilar pair
+        (l3, i3, v3) = rand.choice(ys)
+        out_imgs.append( np.array([ i1, i3 ]) )
+        out_labels.append( 0 )
+
+        sys.stdout.write("\rPairing: %.0f%%" % (i * 100. / args.num))
+        sys.stdout.flush()
+
+    print ""
     print "Write NPZ"
-    xs2_img = np.array([ x for xs2 in res for l, x in xs2 ])
-    xs2_l = np.array([ l for xs2 in res for l, x in xs2 ])
-    np.savez_compressed(args.out_npz, xs2_img, xs2_l)
+    np.savez_compressed(args.out_npz, out_imgs, out_labels)
