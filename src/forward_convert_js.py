ca0d3b40cdea359816660d18ba90a8c97e9ad24d
Axel Angel <axel+git@vneko.ch>
2015-05-12 12:01:46 +0200

    Implement forward_convert_js (with shift_x transform)

---
+++ src/forward_convert_js.py
@@ -0,0 +1,85 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import caffe
+import numpy as np
+import json
+from collections import defaultdict
+from utils import *
+import multiprocessing
+from itertools import izip, chain
+
+if __name__ == "__main__":
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--proto', type=str, required=True)
+    parser.add_argument('--model', type=str, required=True)
+    parser.add_argument('--layer', type=str, default='ip1')
+    parser.add_argument('--in-npz', type=str, required=True)
+    parser.add_argument('--out-js', type=str, required=True)
+    parser.add_argument('--label', type=int, nargs='+', required=True)
+    args = parser.parse_args()
+
+    print "Load model"
+    net = caffe.Net(args.proto, args.model, caffe.TEST)
+    caffe.set_mode_cpu()
+
+    print "Load data"
+    npz = np.load(args.in_npz)
+    X = npz['arr_0']
+    ls = npz['arr_1']
+    Xls = filter(lambda (x, l): l in args.label, np.array([ X, ls ]).T)
+    X = None; ls = None
+    print "Filtered %i images" % (len(Xls))
+
+    # stats per src, label, transformations
+    src_set = dict(dataset=len(Xls))
+    tr_set = dict(identity=len(Xls))
+    label_set = defaultdict(int)
+    for _, l in Xls:
+        label_set[str(l)] += 1
+
+    values = [0, 2, 4, -2, -4]
+    samples = len(Xls) * len(values)
+    def process( (img, l) ):
+        xs = []
+        for v in values:
+            img2 = img_shift_x(img, v)
+            img_caffe = img2.reshape(1, *img2.shape)
+            data = np.asarray([ img_caffe/255. ]) # normalize!
+            out = net.forward_all(data=data, blobs=[ args.layer ])
+            pt = flat_shape(out[args.layer][0])
+            img64 = js_img_encode(img2) # encode64 for JS
+            xs.append( (map(np.asscalar, pt), int(l), v, img64) )
+        return xs
+
+    # forward in parallel
+    print "Processing images"
+    num_cores = multiprocessing.cpu_count()
+    pool = multiprocessing.Pool(num_cores)
+    itr = pool.imap_unordered(process, Xls)
+    info = dict(tr='shift_x', src='dataset')
+    ds = []
+    imgs = []
+    for i, (pt, l, v, img64) in enumerate(chain.from_iterable(itr)):
+        ds.append(dict(x=pt[0], y=pt[1], i=i, l=l, v=v, **info))
+        imgs.append(img64)
+        sys.stdout.write("\rForwarding: %.0f%%" % (i * 100. / samples))
+        sys.stdout.flush()
+    print ""
+
+    # convert to json-serialisable
+    print "Converting into JS"
+    with open(args.out_js, 'wb') as fd:
+        fd.write('var X = ')
+        json.dump(ds, fd)
+        fd.write(';')
+
+        fd.write('var imgs = ')
+        json.dump(imgs, fd)
+        fd.write(';')
+
+        for v in ['src_set', 'label_set', 'tr_set']:
+            fd.write('var %s = ' % (v))
+            json.dump(locals()[v], fd)
+            fd.write(';')
