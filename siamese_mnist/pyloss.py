ae1d30990ae238b8fbee79ad2c58bf5ab1290f72
Axel Angel <axel+git@vneko.ch>
2015-05-14 17:01:46 +0200

    pyloss: Try to replicate LeCun (WIP)

--- siamese_mnist/pyloss.py
+++ siamese_mnist/pyloss.py
@@ -1,11 +1,13 @@
 import caffe
 import numpy as np
 
-class EuclideanLossLayer(caffe.Layer):
+inf = float('inf') # +infinity
+
+class OwnContrastiveLossLayer(caffe.Layer):
 
     def setup(self, bottom, top):
         # check input pair
-        if len(bottom) != 2:
+        if len(bottom) != 3:
             raise Exception("Need two inputs to compute distance.")
 
     def reshape(self, bottom, top):
@@ -13,20 +15,26 @@ class EuclideanLossLayer(caffe.Layer):
         if bottom[0].count != bottom[1].count:
             raise Exception("Inputs must have the same dimension.")
         # difference is shape of inputs
-        self.diff = np.zeros_like(bottom[0].data, dtype=np.float32)
+        self.diff = np.zeros(bottom[0].num, dtype=np.float32)
         # loss output is scalar
         top[0].reshape(1)
 
     def forward(self, bottom, top):
-        self.diff[...] = bottom[0].data - bottom[1].data
-        top[0].data[...] = np.sum(self.diff**2) / bottom[0].num / 2.
+        GW1 = bottom[0].data
+        GW2 = bottom[1].data
+        Y = bottom[2].data
+        DW2 = np.sum( (GW1 - GW2)**2 , axis=1)
+        DW = np.sqrt( DW2 )
+        m = 1.0 # dissimilar margin
+        mdiffmax = np.clip(m - DW2, 0, inf)
+        loss = np.sum( np.multiply(Y, DW2) + np.multiply((1-Y), mdiffmax) )
+        top[0].data[...] = loss / 2.0 / bottom[0].num
+        self.diff[...] = np.multiply(Y, DW) - np.multiply((1-Y), np.clip( m - DW, 0, inf) )
+        print "locals", locals()
+        raise Exception("Stop")
 
     def backward(self, top, propagate_down, bottom):
-        for i in range(2):
-            if not propagate_down[i]:
-                continue
-            if i == 0:
-                sign = 1
-            else:
-                sign = -1
-            bottom[i].diff[...] = sign * self.diff / bottom[i].num
+        for i, sign in enumerate([ +1, -1 ]):
+            if propagate_down[i]:
+                Ndiffs = np.repeat([ self.diff ], bottom[i].channels, axis=0).T
+                bottom[i].diff[...] = sign * Ndiffs / bottom[i].num
