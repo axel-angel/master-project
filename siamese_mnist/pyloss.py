a388a2bf0ad101fcbdec20e4c3f3dbecdf706c51
Axel Angel <axel+git@vneko.ch>
2015-05-15 14:34:03 +0200

    pyloss: Own contrastive loss (vectorial version)

--- siamese_mnist/pyloss.py
+++ siamese_mnist/pyloss.py
@@ -17,6 +17,7 @@ class OwnContrastiveLossLayer(caffe.Layer):
         # difference is shape of inputs
         self.diff = np.zeros(bottom[0].num, dtype=np.float32)
         self.dist_sq = np.zeros(bottom[0].num, dtype=np.float32)
+        self.zeros = np.zeros(bottom[0].num)
         self.m = 1.0
         # loss output is scalar
         top[0].reshape(1)
@@ -27,26 +28,17 @@ class OwnContrastiveLossLayer(caffe.Layer):
         Y = bottom[2].data
         loss = 0.0
         self.diff = GW1 - GW2
-        for i in xrange(bottom[0].num):
-            dist_sq = np.dot(self.diff[i], self.diff[i])
-            self.dist_sq[i] = dist_sq
-            if Y[i]: # similar pairs
-                loss += self.dist_sq[i]
-            else: # dissimilar pair
-                loss += max(0.0, self.m - self.dist_sq[i])
+        self.dist_sq = np.sum(self.diff**2, axis=1)
+        losses = Y * self.dist_sq \
+           + (1-Y) * np.max([self.zeros, self.m - self.dist_sq], axis=0)
+        loss = np.sum(losses)
         top[0].data[0] = loss / 2.0 / bottom[0].num
-        #print "locals", locals()
-        #raise Exception("Stop")
 
     def backward(self, top, propagate_down, bottom):
+        Y = bottom[2].data
+        disClose = np.where(self.m - self.dist_sq > 0.0, 1.0, 0.0)
         for i, sign in enumerate([ +1, -1 ]):
             if propagate_down[i]:
-                alpha = sign * top[0].diff[0] / bottom[i].num
-                for j in xrange(bottom[i].num):
-                    if bottom[2].data[j]: # similar pairs
-                        bottom[i].diff[j,...] = self.diff[j] * +alpha
-                    else: # dissimilar pairs
-                        if self.m - self.dist_sq[j] > 0.0:
-                            bottom[i].diff[j,...] = self.diff[j] * -alpha
-                        else:
-                            bottom[i].diff[j,...] = 0.0
+                alphas = np.where(Y > 0, +1.0, -1.0) * sign * top[0].diff[0] / bottom[i].num
+                facts = ((1-Y) * disClose + Y) * alphas
+                bottom[i].diff[...] = np.array([facts, facts]).T * self.diff
