6c2356d733c850832f18172fe58ee254523cb696
Axel Angel <axel+git@vneko.ch>
2015-05-27 13:49:24 +0200

    Implement forward_dump_data (for gnuplot)

---
+++ src/forward_dump_data.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import caffe
+import numpy as np
+from utils import *
+
+if __name__ == "__main__":
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--proto', type=str, required=True)
+    parser.add_argument('--model', type=str, required=True)
+    parser.add_argument('--layer', type=str, required=True)
+    parser.add_argument('--in-npz', type=str, required=True)
+    parser.add_argument('--out-data', type=str, required=True)
+    args = parser.parse_args()
+
+    print "Load network"
+    net = caffe.Net(args.proto, args.model, caffe.TEST)
+    caffe.set_mode_cpu()
+
+    print "Load data"
+    X = np.load(args.in_npz)
+    xs = X['arr_0']
+    ls = X['arr_1']
+
+    print "Forward output"
+    dims = xs.shape[-2:]
+    res = net.forward_all(data=xs.reshape(-1, 1, *dims), blobs=[args.layer])
+    out = {}
+    for k in res:
+        out[k] = flat_shape(res[k])
+
+    print "Save into GNUplot data"
+    with open(args.out_data, 'w') as fd:
+        for xs in out[args.layer]:
+            fd.write( (" ".join(map(lambda x: "%f" % (x), xs))) + "\n")
