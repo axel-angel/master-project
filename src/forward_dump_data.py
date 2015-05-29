8de869145eb87bbc95e66399c3fa7f931b0bca1a
Axel Angel <axel+git@vneko.ch>
2015-05-29 15:28:01 +0200

    forward_dump_data: Minor var rename, Add index in last column

--- src/forward_dump_data.py
+++ src/forward_dump_data.py
@@ -23,21 +23,21 @@ if __name__ == "__main__":
 
     print "Load data"
     npz = np.load(args.in_npz)
-    xs = npz['arr_0']
+    X = npz['arr_0']
     if args.norb_label:
         ls = [ ii[args.norb_label] for ii in npz['arr_2'] ]
     else:
         ls = npz['arr_1']
 
     print "Forward output"
-    dims = xs.shape[-2:]
-    res = net.forward_all(data=xs.reshape(-1, 1, *dims), blobs=[args.layer])
+    dims = X.shape[-2:]
+    res = net.forward_all(data=X.reshape(-1, 1, *dims), blobs=[args.layer])
     out = {}
     for k in res:
         out[k] = flat_shape(res[k])
 
     print "Save into GNUplot data"
     with open(args.out_data, 'w') as fd:
-        for l, xs in izip(ls, out[args.layer]):
-            cols = map(lambda x: "%f" % (x), xs) + [ str(l) ]
+        for i, l, xs in izip(range(len(ls)), ls, out[args.layer]):
+            cols = map(lambda x: "%f" % (x), xs) + [ str(l), str(i) ]
             fd.write( (" ".join(cols)) + "\n")
