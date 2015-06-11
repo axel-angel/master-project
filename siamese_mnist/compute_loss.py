030b1118f5b5b9bff58edd96c2e7981263398ba1
Axel Angel <axel+git@vneko.ch>
2015-06-11 11:58:46 +0200

    compute_loss: Net surgery for 3D -> 2D (workaround)

--- siamese_mnist/compute_loss.py
+++ siamese_mnist/compute_loss.py
@@ -20,6 +20,14 @@ if __name__ == "__main__":
     net = caffe.Net(args.proto, args.model, caffe.TEST)
     caffe.set_mode_cpu()
 
+    # net surgery (ensure loss uses first 2D)
+    if net.params['feat'][0].data.shape[0] >= 3:
+        print "Set 0 to 3D params"
+        net.params['feat'][0].data[2:,:] = 0
+        net.params['feat'][1].data[2:] = 0
+        net.params['feat_p'][0].data[2:,:] = 0
+        net.params['feat_p'][1].data[2:] = 0
+
     if not args.quiet:
         print "Load dataset"
     npz = np.load(args.in_npz)
@@ -29,7 +37,9 @@ if __name__ == "__main__":
     def process( (x, l) ):
         x_caffe = np.array([ x ]) / 255. # normalize
         l_caffe = np.array([[[[ l ]]]])
-        res = net.forward(pair_data=x_caffe, sim=l_caffe, blobs=[args.layer])
+        res = net.forward(pair_data=x_caffe, sim=l_caffe, blobs=[args.layer, 'feat', 'feat_p'])
+        assert np.all( net.blobs['feat'].data[2:] == 0 )
+        assert np.all( net.blobs['feat_p'].data[2:] == 0 )
         return res[args.layer]
 
     if not args.quiet:
