34bd243a4774d8a710d04521d9c25d02c56fa442
Axel Angel <axel+git@vneko.ch>
2015-06-12 17:25:39 +0200

    siamese_mnist: Update results final_loss

--- siamese_mnist/final_loss_plot.sh
+++ siamese_mnist/final_loss_plot.sh
@@ -1,10 +1,13 @@
+#!/bin/sh
+
 gnuplot <<EOF
 set grid xtics ytics
 set boxwidth 0.9 relative
 set xlabel 'Iterations'
 
 set terminal pngcairo font 'Droid Sans,9' size 500,300
-set title "Loss"
-set output "final_loss_3Dc3-vs-0c_iter300.png"
-plot for [i=2:5] 'final_loss_3Dc3-vs-0c_iter300' u 1:i w linespoints title columnheader(i)
+set title "Loss on LeCun MNIST paired dataset"
+set output "final_loss_test2bv7.png"
+plot 'final_loss_test2bv7_2bv7' u 1:2 w linespoints title "DrLIM", \
+     'final_loss_test2bv7_3Dc3' u 1:2 w linespoints title "Ours"
 EOF
